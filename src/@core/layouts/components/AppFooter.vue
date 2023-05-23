97c18d35230ec2695febb3eecd444f680d693140
Singingsongs <ejesunday2@gmail.com>
2023-05-23 14:53:04 +0100

    fix(Horizontal menu): horizontal menu removed

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,25 +1,19 @@
 <template>
-  <p class="clearfix mb-0">
-    <span class="float-md-left d-block d-md-inline-block mt-25">
-      COPYRIGHT  © {{ new Date().getFullYear() }}
-      <b-link
-        class="ml-25"
-        href="https://1.envato.market/pixinvent_portfolio"
-        target="_blank"
-      >Pixinvent</b-link>
-      <span class="d-none d-sm-inline-block">, All rights Reserved</span>
-    </span>
+	<p class="clearfix mb-0 text-center">
+		<span class="float-md-left d-block d-md-inline-block mt-25">
+			COPYRIGHT © {{ new Date().getFullYear() }}
 
-
-  </p>
+			<span class="d-none d-sm-inline-block">, All rights Reserved</span>
+		</span>
+	</p>
 </template>
 
 <script>
-import { BLink } from 'bootstrap-vue'
+	import { BLink } from "bootstrap-vue";
 
-export default {
-  components: {
-    BLink,
-  },
-}
+	export default {
+		components: {
+			BLink,
+		},
+	};
 </script>
