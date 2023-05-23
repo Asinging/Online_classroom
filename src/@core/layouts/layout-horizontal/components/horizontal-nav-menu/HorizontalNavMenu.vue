eaad0edaebcd0909ff62604882717979a24ff872
Singingsongs <ejesunday2@gmail.com>
2023-05-23 14:09:37 +0100

    setup(admin): admin dashboard done

--- src/@core/layouts/layout-horizontal/components/horizontal-nav-menu/HorizontalNavMenu.vue
+++ src/@core/layouts/layout-horizontal/components/horizontal-nav-menu/HorizontalNavMenu.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="navbar-container main-menu-content">
-    <horizontal-nav-menu-items :items="navMenuItems" />
-  </div>
+	<div class="navbar-container main-menu-content">
+		<!-- <horizontal-nav-menu-items :items="navMenuItems" /> -->
+		<horizontal-nav-menu-items :items="[]" />
+	</div>
 </template>
 
 <script>
-import navMenuItems from '@/navigation/horizontal'
-import HorizontalNavMenuItems from './components/horizontal-nav-menu-items/HorizontalNavMenuItems.vue'
+	// import navMenuItems from '@/navigation/horizontal'
+	import HorizontalNavMenuItems from "./components/horizontal-nav-menu-items/HorizontalNavMenuItems.vue";
 
-export default {
-  components: {
-    HorizontalNavMenuItems,
-  },
-  setup() {
-    return {
-      navMenuItems,
-    }
-  },
-}
+	export default {
+		components: {
+			HorizontalNavMenuItems,
+		},
+		setup() {
+			return {
+				// navMenuItems,
+			};
+		},
+	};
 </script>
 
 <style lang="scss">
-@import "~@core/scss/base/core/menu/menu-types/horizontal-menu.scss";
+	@import "~@core/scss/base/core/menu/menu-types/horizontal-menu.scss";
 </style>
--- src/store/vertical-menu/index.js
+++ src/store/vertical-menu/index.js
@@ -1,15 +1,31 @@
-import { $themeConfig } from '@themeConfig'
+import { $themeConfig } from '@themeConfig';
+import adminNavMenuItems from '@/navigation/vertical/admin';
+import clientNavMenuItems from '@/navigation/vertical/client';
 
 export default {
-  namespaced: true,
-  state: {
-    isVerticalMenuCollapsed: $themeConfig.layout.menu.isCollapsed,
-  },
-  getters: {},
-  mutations: {
-    UPDATE_VERTICAL_MENU_COLLAPSED(state, val) {
-      state.isVerticalMenuCollapsed = val
+    namespaced: true,
+    state: {
+        isVerticalMenuCollapsed: $themeConfig.layout.menu.isCollapsed,
+        adminNavMenuItems: adminNavMenuItems,
+        clientNavMenuItems: clientNavMenuItems
     },
-  },
-  actions: {},
-}
+    mutations: {
+        UPDATE_VERTICAL_MENU_COLLAPSED(state, val) {
+            state.isVerticalMenuCollapsed = val;
+        }
+    },
+    actions: {},
+    getters: {
+        adminAppSideBarMenuListGetter(state) {
+            return state.adminNavMenuItems;
+        },
+        clientAppSideBarMenuListGetter(state) {
+            return state.clientNavMenuItems.map(item => {
+                if (item.title === 'Courses') {
+                    item.tag = '10';
+                }
+                return item;
+            });
+        }
+    }
+};
