c2fa2d0760fd2af539d64c3315a42ae5c5e8a61f
Singingsongs <ejesunday2@gmail.com>
2023-06-12 04:07:11 +0100

    Fix(youtubeSuggestion) : youtube suggestion atempted

--- src/helpers/iframe-helpers/index.js
+++ src/helpers/iframe-helpers/index.js
@@ -1,22 +1,38 @@
 export const checkIframe = item => {
    if (!item || !item.video_url) return false;
-   let str = item.video_url.trim();
+   let str = item.video_url;
 
    let arr = str.split(' ');
 
    if (arr.length < 2) {
+      item.video_url = _itemManipulate(item.video_url);
       return item;
    }
-
-   arr = arr.filter(item => {
-      return !item.includes('height') && !item.includes('width');
-   });
+   arr = arr
+      .filter(item => {
+         return !item.includes('height') && !item.includes('width');
+      })
+      .map(item => {
+         if (item.includes('src')) {
+            return _itemManipulate(item);
+         }
+         return item;
+      });
    arr.splice(2, 0, 'width="1200"');
    str = arr.reduce((prev, curr) => {
       return prev + ' ' + curr;
    });
-   debugger;
    item.isIframe = true;
    item.video_url = str;
    return item;
+};
+
+const _itemManipulate = item => {
+   let arr = item.split('/');
+   let lastEle = arr.slice(-1);
+   lastEle = lastEle + '?rel=0';
+   arr.splice(arr.length - 1, 1, lastEle);
+   let str = arr.reduce((prev, ele) => prev + '/' + ele);
+
+   return str;
 };
